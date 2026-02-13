3199c3e736014d9187d412edcbf51faec1da0b6c
Kiran Wood <kiranhonradowood@gmail.com>
2026-02-12 21:04:44 -0800

    Working on box collision with circle

--- EngineXForStudent/Game/Private/Components/BoxColliderComponent.cpp
+++ EngineXForStudent/Game/Private/Components/BoxColliderComponent.cpp
@@ -1,4 +1,7 @@
 #include "Game/Public/Components/BoxColliderComponent.h"
+#include "Game/Public/Components/CircleColliderComponent.h"
+#include "Game/Public/Components/TransformComponent.h"
+#include "Game/Public/Actor.h"
 
 BoxColliderComponent::BoxColliderComponent(std::weak_ptr<Actor> owner, float width, float height, exVector2 velocity, bool isStatic, bool isGravityEnabled)
 	: PhysicsComponent(owner, velocity, isStatic, isGravityEnabled),
@@ -11,9 +14,42 @@ bool BoxColliderComponent::IsCollisionDetected(std::weak_ptr<PhysicsComponent>& 
 	// Checks that other component exists
 	if (!otherComponent.expired())
 	{
-		if (std::shared_ptr )
+		if (std::shared_ptr<CircleColliderComponent> OtherCircleColliderComponent = std::dynamic_pointer_cast<CircleColliderComponent>(otherComponent.lock()))
+		{
+			exVector2 selfCenterPos;
+			exVector2 otherCenterPos;
+
+			if (!mOwner.expired())
+			{
+				if (const std::shared_ptr<TransformComponent> TransformComp = mOwner.lock()->GetComponentOfType<TransformComponent>())
+				{
+					selfCenterPos = TransformComp->GetLocation();
+				}
+			}
+
+			if (!otherComponent.lock()->GetOwner().expired())
+			{
+				if (const std::shared_ptr<TransformComponent> TransformComp = OtherCircleColliderComponent->GetOwner().lock()->GetComponentOfType<TransformComponent>())
+				{
+					otherCenterPos = TransformComp->GetLocation();
+				}
+			}
+
+			exVector2 testPos;
+
+			// Calculates which side of the square the circle is colliding with
+			if (otherCenterPos.x < selfCenterPos.x) testPos.x = selfCenterPos.x;
+			else if (otherCenterPos.x > (selfCenterPos.x + mWidth)) testPos.x = selfCenterPos.x + mWidth;
+
+			if (otherCenterPos.y < selfCenterPos.y) testPos.y = selfCenterPos.y;
+			
+		}
 	}
 
 	return false;
+}
+
+void BoxColliderComponent::CollisionResolution()
+{
 }
 
